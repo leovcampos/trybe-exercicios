cb09dc213840869ec4a45d8cfb1e2e1e581d761a
leovcampos <leonardoviniciuscampos@gmail.com>
2022-04-07 19:52:11 -0300

    completo

--- fundamentos/bloco-4-introduçao-a-javascript-e-logica-de-programaçao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introduçao-a-javascript-e-logica-de-programaçao/dia-1-javascript-primeiros-passos/script.js
@@ -133,9 +133,9 @@ if(number1 % 2 == 0 || number2 % 2 == 0 || number3 % 2 == 0){
 // 9.Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
 // .Bonus: use somente um if 
 
-const number5 = 10;
-const number6 = 55;
-const number7 = 2;
+const number1 = 10;
+const number2 = 55;
+const number3 = 2;
 
 if(number1 % 2 == 1 || number2 % 2 == 1 || number3 % 2 == 1){
     console.log("true")
