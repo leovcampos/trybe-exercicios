1434a6fe332ac58eeb23d06ee6de032a658c1b0b
leovcampos <leonardoviniciuscampos@gmail.com>
2022-04-12 15:04:39 -0300

    exercicio objetos

---
+++ fundamentos/bloco-4-introduçao-a-javascript-e-logica-de-programaçao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -0,0 +1,62 @@
+//exercicios objetos e For/in do 1 ao 5:
+
+/*let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  console.log('Bem vinda, ' + info.personagem);
+  
+  info['recorrente'] = 'Sim';
+  
+  console.log(info);
+
+  for(let object in info){
+      console.log(object);
+  }
+
+  for(let object in info){
+    console.log(info[object]);
+}
+
+info['personagem'] += ' e Tio Patinhas';
+info['origem'] += ' e Christmas on Bear Mountain, Dells Four Color Comics #178';
+info['nota'] += ' e O último MacPatinhas';
+info['recorrente'] = 'Ambos recorrentes';
+
+console.log(info);*/
+
+//exercicios objetos e For/in do 6 ao 8:
+
+
+/*let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+
+
+  console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0]['titulo'] + '.');
+
+  leitor['livrosFavoritos'].push({
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  })
+
+  console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos.')*/
+
+  
+
+
+
+
