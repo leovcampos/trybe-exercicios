309a39708b05b41ea045bd2511144112406e9e41
leovcampos <leonardoviniciuscampos@gmail.com>
2022-04-15 19:48:46 -0300

    exercicios 5.2

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -0,0 +1,19 @@
+let valueA = [];
+let valueB = [];
+let Iguais = [];
+
+function includesArrays(a, b){
+    for( let i = 0; i < a.length; i += 1){
+        valueA.push(a[i]);
+        return a 
+    }
+    for( let u = 0; u < b.length; u += 1){
+        valueB.push(b[u]);
+        return b
+    }
+    if(a[i] === b[u]){
+        Iguais.push();
+    }
+  }
+
+  includesArrays([1, 2, 3], [4, 2, 4]);
