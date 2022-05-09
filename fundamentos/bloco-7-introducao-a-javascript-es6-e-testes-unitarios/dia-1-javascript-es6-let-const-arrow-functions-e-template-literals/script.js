6f3402f09b70b1951bd372b57a2ef9311bf9921c
leovcampos <leonardoviniciuscampos@gmail.com>
2022-05-09 12:04:08 -0300

    exercicios

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,3 +1,5 @@
+// Exercicio parte | número 1
+
 const testingScope = (escopo) => {
     if (escopo === true) {
       var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -12,4 +14,23 @@ const testingScope = (escopo) => {
 
   testingScope(true);
 
-  
+// Exercicio parte | número 2
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+  //oddsAndEvens.sort((a, b) => a -b);
+  console.log(`A ordem crescente deste array é ${oddsAndEvens.sort((a, b) => a -b)}`);
+
+// Exercicio parte || número 1
+
+const fatorial = n => n > 1 ? n * fatorial(n - 1) : n;
+console.log(fatorial(3));
+
+// Exercicio parte || número 2
+
+longestWord('Antônio foi no banheiro e não sabemos o que aconteceu') // retorna 'aconteceu'
+
+let bigger = ''
+
+longestWord.split(' ').forEach(word => {
+  if
+})
