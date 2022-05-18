dc977009fb626931b55f82427367e6e93f681c5e
leovcampos <leonardoviniciuscampos@gmail.com>
2022-05-18 13:56:34 -0300

    exercicios finalizados

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercicio1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercicio1.js
@@ -68,9 +68,7 @@ const books = [
 function formatedBookNames(array) {
     return array.map((element) => `${element.name} - ${element.genre} - ${element.author.name}`);
 }
-// console.log(formatedBookNames(books));
-
-// Ex 2: Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author, com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+// console.log(fnew Date();a um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author, com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
 
 function nameAndAge(array) {
     return array.map((element) => (
@@ -88,6 +86,37 @@ function nameAndAge(array) {
 function fantasyOrScienceFiction(array) {
     return array.filter(element => element.genre === 'Ficção Científica' || element.genre === 'Fantasia');
 }
-// console.log(fantasyOrScienceFiction(books));
+//console.log(fantasyOrScienceFiction(books));
 
+// Ex 4: Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
 
+function oldBooksOrdered(array) {
+    return array.filter((element => new Date().getFullYear() - element.releaseYear > 60))
+    .sort((a, b) => a.releaseYear - b.releaseYear);
+  }
+// console.log(oldBooksOrdered(books));
+
+// Ex 5: Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
+
+function fantasyOrScienceFictionAuthors(array) {
+    return array.filter(element => element.genre === 'Ficção Científica' || element.genre === 'Fantasia')
+    .map(element => element.author.name)
+    .sort();
+  }
+// console.log(fantasyOrScienceFictionAuthors(books));
+
+// Ex 6: Crie um array com o nome de todos os livros com mais de 60 anos de publicação.
+
+function oldBooks(array) {
+    return array.filter((element => new Date().getFullYear() - element.releaseYear > 60))
+    .map((element => element.name))
+  }
+// console.log(oldBooks(books));
+
+// Ex 7: Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
+
+function authorWith3DotsOnName(array) {
+    return array.find(element => element.author.name.split(' ')
+    .filter(word => word.endsWith('.')).length === 3).name;
+  }
+// console.log(authorWith3DotsOnName(books));  
