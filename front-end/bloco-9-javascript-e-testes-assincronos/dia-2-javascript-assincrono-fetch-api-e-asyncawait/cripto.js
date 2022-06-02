5bae4828aa82e852d6d91200dd23697b5201983a
leovcampos <leonardoviniciuscampos@gmail.com>
2022-06-01 22:47:22 -0300

    praticando async

---
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/cripto.js
@@ -0,0 +1,43 @@
+// const fetch = require('node-fetch');
+
+const criptoList = async () => {
+    const url = 'https://api.coincap.io/v2/assets';
+    
+    const coins = await fetch(url)
+    .then(response => response.json())
+    .then(data => data.data)
+    .catch(error => error.toString('Deu erro!'))   
+
+    return coins;
+}
+
+const fetchUsdCurrencies = async () => {
+    const url = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest';
+    const usdEndPoint = '/currencies/usd.min.json';
+    const urlUsd = url.concat(usdEndPoint);
+
+    const usdCurrencies = await fetch(urlUsd)
+    .then((response) => response.json())
+    .then((data) => data.usd)
+    .catch((error) => error.toString('Deu erro!'));
+
+    return usdCurrencies;
+}
+
+const setCoins = async () => {
+    const coins = await criptoList();
+    const localCoins = document.querySelector('#coins-list');
+    const usdCurrencies = await fetchUsdCurrencies();
+
+    coins.filter((element, index) => index < 10)
+    .forEach((coin) => {
+        const li = document.createElement('li');
+        const price = Number(coin.priceUsd);
+        const priceBr = price * usdCurrencies.brl;
+
+        li.innerText = `${coin.name} (${coin.symbol}): R$${priceBr.toFixed(2)} // US$${price.toFixed(2)}`;
+        localCoins.appendChild(li);
+    })
+}
+setCoins();
+
